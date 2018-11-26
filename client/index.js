7297d24f2efca7c052613da14ea1d1ad6afe52b4
lukechadwick <llukechadwick@gmail.com>
2018-11-26 23:43:30 +1300

    set up grid system

--- client/index.js
+++ client/index.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import App from './components/App';
 
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
+
 render(
 	<Router>
 		<Switch>
