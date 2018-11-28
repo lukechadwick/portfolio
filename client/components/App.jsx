d0cc8cdc93ea420a9f7abc5ddda5c3f071ab5228
lukechadwick <llukechadwick@gmail.com>
2018-11-28 16:20:01 +1300

    horizontal stacking and scanner

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,37 @@ import Navbar from './Navbar';
 import Grid from './Grid';
 
 class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar />
-				<Grid />
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div class="ocrloader">
+          <em />
+          <div>
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+            <i />
+          </div>
+          <span />
+        </div>{' '}
+        <Grid />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- client/components/Grid.jsx
+++ client/components/Grid.jsx
@@ -6,160 +6,136 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 import Card from './Card';
 
 class Grid extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentTitle: '',
-			currentContent: ''
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTitle: '',
+      currentContent: ''
+    };
+  }
 
-	onLayoutChange = (layout, layouts) => {
-		console.log('layout', layout);
+  onLayoutChange = (layout, layouts) => {
+    console.log('layout', layout);
 
-		layout.forEach(e => {
-			console.log(e);
-			if ((e.x == 0) & (e.y == 0)) {
-				console.log(e.i);
-				this.setState({ currentTitle: e.i });
-			}
-		});
-	};
+    layout.forEach(e => {
+      console.log(e);
+      if ((e.x == 0) & (e.y == 0)) {
+        console.log(e.i);
+        this.setState({ currentTitle: e.i });
+      }
+    });
+  };
 
-	render() {
-		let layouts = {
-			lg: [
-				{
-					w: 6,
-					h: 1,
-					x: 0,
-					y: 1,
-					i: 'Shelf',
-					static: true
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-				},
-				{
-					w: 2,
-					h: 2,
-					x: 0,
-					y: 0,
-					i: 'Card'
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-					// static: false
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 0,
-					y: 0,
-					i: 'Tools',
-					isResizable: false
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-					// static: false
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 1,
-					y: 0,
-					i: 'Contact Me',
-					isResizable: false
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-					// static: false
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 2,
-					y: 0,
-					i: 'Projects',
-					isResizable: false
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-					// static: false
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 3,
-					y: 0,
-					i: 'Me',
-					isResizable: false
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-					// static: false
-				}
-			]
-		};
+  render() {
+    let layouts = {
+      lg: [
+        {
+          w: 6,
+          h: 1,
+          x: 0,
+          y: 1,
+          i: 'Shelf',
+          static: true
+        },
+        {
+          w: 3,
+          h: 2,
+          x: 0,
+          y: 3,
+          i: 'Card'
+          // minW: 2,
+          // maxW: 2,
+          // minH: 2,
+          // maxH: 2,
+          // moved: false,
+          // static: false
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 0,
+          y: 0,
+          i: 'Tools',
+          isResizable: false,
+          compactType: 'horizontal'
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 1,
+          y: 0,
+          i: 'Contact Me',
+          isResizable: false,
+          compactType: 'horizontal'
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 2,
+          y: 0,
+          i: 'Projects',
+          isResizable: false,
+          compactType: 'horizontal'
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 3,
+          y: 0,
+          i: 'Me',
+          isResizable: false,
+          compactType: 'horizontal'
+        }
+      ]
+    };
 
-		return (
-			<ResponsiveGridLayout
-				className="layout"
-				layouts={layouts}
-				breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-				cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-				onLayoutChange={(layout, layouts) =>
-					this.onLayoutChange(layout, layouts)
-				}
-			>
-				<div className="box" key="Shelf">
-					<img
-						className="center gridImage"
-						draggable="false"
-						src="shelf2.png"
-					/>
-				</div>
-				<div className="box" key="Card">
-					<Card
-						title={this.state.currentTitle}
-						content={this.state.currentContent}
-					/>
-				</div>
-				<div className="box" key="Tools">
-					<img className="center gridImage" draggable="false" src="tools.png" />
-				</div>
-				<div className="box" key="Contact Me">
-					<img
-						className="center gridImage"
-						draggable="false"
-						src="addressbook.png"
-					/>
-				</div>
-				<div className="box" key="Projects">
-					<img className="center gridImage" draggable="false" src="code.png" />
-				</div>
-				<div className="box" key="Me">
-					<img
-						className="center gridImage me"
-						draggable="false"
-						src="me.jpeg"
-					/>
-				</div>
-			</ResponsiveGridLayout>
-		);
-	}
+    return (
+      <ResponsiveGridLayout
+        className="layout"
+        layouts={layouts}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        onLayoutChange={(layout, layouts) =>
+          this.onLayoutChange(layout, layouts)
+        }
+        compactType="horizontal"
+      >
+        <div className="box" key="Shelf">
+          <img
+            className="center gridImage"
+            draggable="false"
+            src="shelf2.png"
+          />
+        </div>
+        <div className="box" key="Card">
+          <Card
+            title={this.state.currentTitle}
+            content={this.state.currentContent}
+          />
+        </div>
+        <div className="box" key="Tools">
+          <img className="center gridImage" draggable="false" src="tools.png" />
+        </div>
+        <div className="box" key="Contact Me">
+          <img
+            className="center gridImage"
+            draggable="false"
+            src="addressbook.png"
+          />
+        </div>
+        <div className="box" key="Projects">
+          <img className="center gridImage" draggable="false" src="code.png" />
+        </div>
+        <div className="box" key="Me">
+          <img
+            className="center gridImage me"
+            draggable="false"
+            src="me.jpeg"
+          />
+        </div>
+      </ResponsiveGridLayout>
+    );
+  }
 }
 
 export default Grid;
