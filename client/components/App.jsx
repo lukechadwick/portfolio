9da10cd584bd15d278da9f6fc630aa48957bf556
lukechadwick <llukechadwick@gmail.com>
2018-11-29 01:02:01 +1300

    add dynamic rendering of cards

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,18 +3,20 @@ import Navbar from './Navbar';
 import Grid from './Grid';
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <div class="ocrloader">
-          <em />
-          <span />
-        </div>
-        <Grid />
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar />
+				{/* <div className="container"> */}
+				<div className="ocrloader">
+					<em />
+					<span />
+				</div>
+				<Grid />
+				{/* </div> */}
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -10,11 +10,53 @@ class Card extends Component {
 			<div className="card bg-light mb-3">
 				<div className="card-header">{this.props.title}</div>
 				<div className="card-body">
-					<h5 className="card-title">Luke Chadwick</h5>
-					<p className="card-text">
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</p>
+					{this.props.title == 'Me' && (
+						<React.Fragment>
+							<h5 className="card-title">Luke Chadwick</h5>
+							<p className="card-text">
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</p>
+						</React.Fragment>
+					)}
+					{this.props.title == 'Contact Me' && (
+						<React.Fragment>
+							<h5 className="card-title center">Email, Linked-in or GitHub</h5>
+							<div className="center">
+								<a href="www.google.com" target="blank">
+									<img
+										draggable="false"
+										src="email.png"
+										className="toolImage"
+									/>
+								</a>
+								<a href="www.google.com" target="blank">
+									<img
+										draggable="false"
+										src="linkedin.png"
+										className="toolImage"
+									/>
+								</a>
+								<a href="www.google.com" target="blank">
+									<img
+										draggable="false"
+										src="github.png"
+										className="toolImage"
+									/>
+								</a>
+							</div>
+						</React.Fragment>
+					)}
+					{this.props.title == 'Tools' && (
+						<React.Fragment>
+							<p>Tools</p>
+						</React.Fragment>
+					)}
+					{this.props.title == 'Projects' && (
+						<React.Fragment>
+							<p>Projects</p>
+						</React.Fragment>
+					)}
 				</div>
 			</div>
 		);
