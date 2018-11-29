52d8924afe5fee9cb2979a64493671b8d24c56b0
lukechadwick <llukechadwick@gmail.com>
2018-11-29 15:38:09 +1300

    tools

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,66 +1,75 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<div className="card bg-light mb-3">
-				<div className="card-header">{this.props.title}</div>
-				<div className="card-body">
-					{this.props.title == 'Me' && (
-						<React.Fragment>
-							<h5 className="card-title">Luke Chadwick</h5>
-							<p className="card-text">
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-						</React.Fragment>
-					)}
-					{this.props.title == 'Contact Me' && (
-						<React.Fragment>
-							<h5 className="card-title center">Email, Linked-in or GitHub</h5>
-							<div className="center">
-								<a href="www.google.com" target="blank">
-									<img
-										draggable="false"
-										src="email.png"
-										className="toolImage"
-									/>
-								</a>
-								<a href="www.google.com" target="blank">
-									<img
-										draggable="false"
-										src="linkedin.png"
-										className="toolImage"
-									/>
-								</a>
-								<a href="www.google.com" target="blank">
-									<img
-										draggable="false"
-										src="github.png"
-										className="toolImage"
-									/>
-								</a>
-							</div>
-						</React.Fragment>
-					)}
-					{this.props.title == 'Tools' && (
-						<React.Fragment>
-							<p>Tools</p>
-						</React.Fragment>
-					)}
-					{this.props.title == 'Projects' && (
-						<React.Fragment>
-							<p>Projects</p>
-						</React.Fragment>
-					)}
-				</div>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="card bg-light mb-3">
+        <div className="card-header">
+          <h4>{this.props.title}</h4>
+        </div>
+        <div className="card-body">
+          {this.props.title == 'Me' && (
+            <React.Fragment>
+              <h5 className="card-title">Luke Chadwick</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </React.Fragment>
+          )}
+          {this.props.title == 'Contact Me' && (
+            <React.Fragment>
+              <h5 className="card-title center">Email, Linked-in or GitHub</h5>
+              <div className="center">
+                <a href="www.google.com" target="blank">
+                  <img
+                    draggable="false"
+                    src="email.png"
+                    className="contactImage"
+                  />
+                </a>
+                <a href="www.google.com" target="blank">
+                  <img
+                    draggable="false"
+                    src="linkedin.png"
+                    className="contactImage"
+                  />
+                </a>
+                <a href="www.google.com" target="blank">
+                  <img
+                    draggable="false"
+                    src="github.png"
+                    className="contactImage"
+                  />
+                </a>
+              </div>
+            </React.Fragment>
+          )}
+          {this.props.title == 'Tools' && (
+            <React.Fragment>
+              <h4 className="card-title center">The Tools of my trade</h4>
+              <img draggable="false" src="js.png" className="toolImage" />
+              <img draggable="false" src="node.png" className="toolImage" />
+              <img draggable="false" src="react.png" className="toolImage" />
+              <img draggable="false" src="html.png" className="toolImage" />
+              <img draggable="false" src="css.png" className="toolImage" />
+              <img draggable="false" src="csharp.png" className="toolImage" />
+              <img draggable="false" src="rails.png" className="toolImage" />
+            </React.Fragment>
+          )}
+          {this.props.title == 'Projects' && (
+            <React.Fragment>
+              <p>Projects</p>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
--- client/components/Grid.jsx
+++ client/components/Grid.jsx
@@ -6,151 +6,151 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 import Card from './Card';
 
 class Grid extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentTitle: ''
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTitle: ''
+    };
+  }
 
-	onLayoutChange = (layout, layouts) => {
-		console.log('layout', layout);
+  onLayoutChange = (layout, layouts) => {
+    console.log('layout', layout);
 
-		layout.forEach(e => {
-			console.log(e);
-			if ((e.x == 0) & (e.y == 0)) {
-				console.log(e.i);
-				this.setState({ currentTitle: e.i });
-			}
-		});
+    layout.forEach(e => {
+      console.log(e);
+      if ((e.x == 0) & (e.y == 0)) {
+        console.log(e.i);
+        this.setState({ currentTitle: e.i });
+      }
+    });
 
-		console.log(this.containerLine.height);
-	};
+    console.log(this.containerLine.height);
+  };
 
-	render() {
-		let layouts = {
-			lg: [
-				{
-					w: 6,
-					h: 1,
-					x: 0,
-					y: 1,
-					i: 'Shelf',
-					static: true
-				},
-				{
-					w: 4,
-					h: 2,
-					x: 0,
-					y: 3,
-					i: 'Card'
-					// minW: 2,
-					// maxW: 2,
-					// minH: 2,
-					// maxH: 2,
-					// moved: false,
-					// static: false
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 0,
-					y: 0,
-					i: 'Tools',
-					isResizable: false,
-					compactType: 'horizontal'
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 1,
-					y: 0,
-					i: 'Contact Me',
-					isResizable: false,
-					compactType: 'horizontal'
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 2,
-					y: 0,
-					i: 'Projects',
-					isResizable: false,
-					compactType: 'horizontal'
-				},
-				{
-					w: 1,
-					h: 1,
-					x: 3,
-					y: 0,
-					i: 'Me',
-					isResizable: false,
-					compactType: 'horizontal'
-				}
-			]
-		};
+  render() {
+    let layouts = {
+      lg: [
+        {
+          w: 6,
+          h: 1,
+          x: 0,
+          y: 1,
+          i: 'Shelf',
+          static: true
+        },
+        {
+          w: 6,
+          h: 2,
+          x: 0,
+          y: 3,
+          i: 'Card'
+          // minW: 2,
+          // maxW: 2,
+          // minH: 2,
+          // maxH: 2,
+          // moved: false,
+          // static: false
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 0,
+          y: 0,
+          i: 'Tools',
+          isResizable: false,
+          compactType: 'horizontal'
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 1,
+          y: 0,
+          i: 'Contact Me',
+          isResizable: false,
+          compactType: 'horizontal'
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 2,
+          y: 0,
+          i: 'Projects',
+          isResizable: false,
+          compactType: 'horizontal'
+        },
+        {
+          w: 1,
+          h: 1,
+          x: 3,
+          y: 0,
+          i: 'Me',
+          isResizable: false,
+          compactType: 'horizontal'
+        }
+      ]
+    };
 
-		return (
-			<React.Fragment>
-				<ResponsiveGridLayout
-					className="layout"
-					layouts={layouts}
-					breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-					cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 4 }}
-					onLayoutChange={(layout, layouts) =>
-						this.onLayoutChange(layout, layouts)
-					}
-					compactType="horizontal"
-				>
-					<div className="box" key="Shelf">
-						<img
-							className="center gridImage shelf"
-							draggable="false"
-							src="shelf2.png"
-						/>
-						<h2 className="center" style={{ color: 'white' }}>
-							Drag an icon into the scanner to find out more...
-						</h2>
-					</div>
-					<div className="box" key="Card">
-						<Card
-							title={this.state.currentTitle}
-							content={this.state.currentContent}
-						/>
-					</div>
-					<div className="box" key="Tools">
-						<img
-							className="center gridImage"
-							draggable="false"
-							src="tools.png"
-						/>
-					</div>
-					<div className="box" key="Contact Me">
-						<img
-							className="center gridImage"
-							draggable="false"
-							src="addressbook.png"
-							ref={el => (this.containerLine = el)}
-						/>
-					</div>
-					<div className="box" key="Projects">
-						<img
-							className="center gridImage"
-							draggable="false"
-							src="code.png"
-						/>
-					</div>
-					<div className="box" key="Me">
-						<img
-							className="center gridImage me"
-							draggable="false"
-							src="me.jpeg"
-						/>
-					</div>
-				</ResponsiveGridLayout>
-			</React.Fragment>
-		);
-	}
+    return (
+      <React.Fragment>
+        <ResponsiveGridLayout
+          className="layout"
+          layouts={layouts}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 4 }}
+          onLayoutChange={(layout, layouts) =>
+            this.onLayoutChange(layout, layouts)
+          }
+          compactType="horizontal"
+        >
+          <div className="box" key="Shelf">
+            <img
+              className="center gridImage shelf"
+              draggable="false"
+              src="shelf2.png"
+            />
+            <h2 className="center" style={{ color: 'white' }}>
+              Drag an icon into the scanner to find out more...
+            </h2>
+          </div>
+          <div className="box" key="Card">
+            <Card
+              title={this.state.currentTitle}
+              content={this.state.currentContent}
+            />
+          </div>
+          <div className="box" key="Tools">
+            <img
+              className="center gridImage"
+              draggable="false"
+              src="tools.png"
+            />
+          </div>
+          <div className="box" key="Contact Me">
+            <img
+              className="center gridImage"
+              draggable="false"
+              src="addressbook.png"
+              ref={el => (this.containerLine = el)}
+            />
+          </div>
+          <div className="box" key="Projects">
+            <img
+              className="center gridImage"
+              draggable="false"
+              src="code.png"
+            />
+          </div>
+          <div className="box" key="Me">
+            <img
+              className="center gridImage me"
+              draggable="false"
+              src="me.jpeg"
+            />
+          </div>
+        </ResponsiveGridLayout>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Grid;
