4214dab29c1de8517e4876d4cdc6ea8d80f0a7fc
lukechadwick <llukechadwick@gmail.com>
2019-02-23 10:56:44 +1300

    tidy

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Grid from './Grid';
-import ParallaxScroll from './Parallax';
+// import ParallaxScroll from './Parallax';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        {/* <div className="containerCenter"> */}
-        {/* <div className="ocrloader">
-            <em />
-            <span />
-          </div> */}
         <div className='container'>
           <Grid />
         </div>
         {/* <ParallaxScroll /> */}
-
-        {/* </div> */}
       </React.Fragment>
     );
   }
