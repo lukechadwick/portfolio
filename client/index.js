c72d7056490e68d2137cb12ed2b063c5e35dd62b
lukechadwick <llukechadwick@gmail.com>
2019-02-24 15:16:54 +1300

    remove unused import

--- client/index.js
+++ client/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-
 import App from './components/App';
 
 import '../node_modules/react-grid-layout/css/styles.css';
@@ -10,9 +7,7 @@ import '../node_modules/react-resizable/css/styles.css';
 
 render(
   <React.Fragment>
-    {/* <Switch> */}
     <App />
-    {/* </Switch> */}
   </React.Fragment>,
   document.getElementById('app')
 );
